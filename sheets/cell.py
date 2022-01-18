fc5aca2e5c53e00e303b4c85e95d2b79a1fef81c
Pieter Van Santvliet <pietervs1@hotmail.com>
2022-01-17 22:59:42 -0800

    clean up formula evaluation code

--- sheets/cell.py
+++ sheets/cell.py
@@ -1,6 +1,7 @@
 # Object class for individual cell
 
 import lark
+from eval_expressions import EvalExpressions
 from .cell_error import CellErrorType, CellError
 
 class Cell:
@@ -9,100 +10,27 @@ class Cell:
         # self.contents = contents
 
         if contents[0] == '=':
-            ''' this is probably not useful
-            #check to make sure no obvious errors
-            
-            # if an error is encountered
-            if contents.find('/0'):
-                error = CellError(CellErrorType.DIVIDE_BY_ZERO, "Divide By Zero", "None")
-                self.value = error
-                
-            else:
-            '''
-
             self.type = "FORMULA"
             self.value = self.get_value_from_contents(contents)
 
         elif contents[0] == "'":
             self.type = "STRING"
             #self.value = str(contents)
+
         else:
             self.type = "LITERAL"
             #self.value = contents
 
 
-    def get_value_from_contents(self, contents): # assume that contents is '=a1'  '=aa234 + 4 - b1'
+    def get_value_from_contents(self, contents):
         parser = lark.Lark.open('sheets/formulas.lark', start='formula')
-        formula = parser.parse(contents).pretty()
 
-        return 0 # temporary
+        try:
+            formula = parser.parse(contents)
+        except:
+            print('error') # TODO error
+            exit()
 
-        ''' old code for formula evaluation
-        #Parsing formula and pushing operations onto stack inside out
-        stack = []
-        for i in formula.splitlines(): 
-            if len(stack) > 0:
-                prev = stack[-1]
-            
-            if i == 'add_expr':
-                stack.append()
-                pass
-            elif i == 'mul_expr':
-                stack.append('*')
-            elif i == 'number':
-                pass    
-            elif i == 'cell':
-                # TODO check to make sure that the cell reference is valid, if not error
-                
-                # get_cell_contents
-                pass     
-            elif i == 'concat_expr':
-                pass
-            elif i == 'string':
-                pass
-            elif i == 'parens':
-                pass
-            elif i == 'unary_op':
-                pass
-            
-            # TODO binary operators
-
-            else:
-                print('houston, we have a problem')
-                pass
+        evaluation = EvalExpressions().transform(formula)
         
-        #Pop off stack and calculate formula
-        while (len(stack) > 0):
-            pass
-                
-
-        # add_expr
-        #   cell  A1
-        #   +
-        #   mul_expr
-        #     number      3
-        #     *
-        #     cell
-        #       test
-        #       A2
-
-
-        for i in contents:
-            if i == '=':
-                continue
-            elif i == ' ':
-                continue
-            # elif i ==  "'":
-            #     # some string will be
-            # elif i.isdigit():
-            #     skip all the way to the end until no ore digits
-            # elif i == string Char 
-
-            #     a13+
-            #     balance!a13+
-            #     get every character following it that is a string and a number until there is somethign that isnt a number
-
-        
-
-        # balances!a3
-        '''
+        return evaluation
--- sheets/eval_expressions.py
+++ sheets/eval_expressions.py
@@ -1,14 +1,69 @@
-import os
-clear = lambda: os.system('cls'); clear() # clear the command window
-print('################## NEW RUN ##################\n')
+# object class for evaluating expressions
 
 import lark
-from lark import Tree, Transformer
+from lark import Transformer
 
+# used to evaluate an expression from the parsed formula
+class EvalExpressions(Transformer):
+
+    def number(self, args):
+        return args[0]
+
+    def string(self, args):
+        return args[0][1:-1] # the '[1:-1]' is to remove the double quotes
+
+    def unary_op(self, args):
+        return str(args[0]+args[1])
+
+    def parens(self, args):
+        return args[0]
+
+    def add_expr(self, args):
+        t = str(args[0])+args[1]+str(args[2])
+
+        return eval(t)
+
+    def mul_expr(self, args):
+        t = str(args[0])+args[1]+str(args[2])
+
+        return eval(t)
+
+    def concat_expr(self, args):
+        return str(args[0]+args[1])
+
+    def cell(self, args):
+        # getting the appropriate sheet name and cell location
+        if len(args) == 1:      # if using the current sheet
+            sheet_name = 'current' # TODO fix
+            cell = args[0]
+        elif len(args) == 2:    # if using a different sheet
+            # in case of quotes around sheet name
+            if args[0][0] == "'" and args[0][-1] == "'":
+                sheet_name = args[0][1:-1]
+            elif not args[0][0] == "'" and not args[0][-1] == "'":
+                sheet_name = args[0]
+            else:
+                pass # TODO error
+            cell = args[1]
+        else:
+            pass # TODO error
+
+        cell_value = 1 # TODO get_cell_value(sheet_name, cell) here
+
+        if cell_value == 'None':
+            cell_value = 0 # TODO "" for string
+
+        return cell_value
+
+""" 
+import os; clear = lambda: os.system('cls'); clear() # clear the command window
+
+# testing options
+contents = "=5+4*(6+5)/5*-1-4"
 # contents = '="hello" & "world" & "test" & 6 & 4+3'
 # contents = '="test"'
 # contents = "='test_ 9'!aa33+3"
-contents = "='test_ 9'!aa33+3"
+# contents = "='test_ 9'!aa33+3"
 # contents = '="hello"'
 # contents = "='hello'"
 # contents = '=a1'
@@ -21,89 +76,9 @@ except:
     print('error') # TODO error
     exit()
 
-# used to evaluate an expression from the parsed formula
-class EvalExpressions(Transformer):
-    # def __init__(self):
-
-
-
-    def mul_expr(self, args):
-        t = str(args[0])+args[1]+str(args[2])
-
-        # print('args:',args)
-        # print('t:',t)
-
-        return eval(t)
-
-    def number(self, args):
-
-        # print('number args:', args[0])
-        
-        return args[0]
-
-    def add_expr(self, args):
-
-        t = str(args[0])+args[1]+str(args[2])
-
-        # print('add args:',args)
-        # print('t:',t)
-
-        return eval(t)
-
-    def cell(self, args):
-        if len(args) == 1:
-            sheet_name = 'current' 
-            cell = args[0]
-        elif len(args) == 2:
-            if args[0][0] == "'" and args[0][-1] == "'":
-                sheet_name = args[0][1:-1]
-            elif not args[0][0] == "'" and not args[0][-1] == "'":
-                sheet_name = args[0]
-            else:
-                pass # TODO error
-            cell = args[1]
-        else:
-            pass # TODO error
-
-        print('sheet:', sheet_name)
-        print('cell:', cell)
-
-        cell_value = 1 # TODO get_cell_value(sheet_name, cell)
-
-        if cell_value == 'None':
-            cell_value = 0 # TODO "" for string
-
-        return cell_value
-
-    def parens(self, args):
-        return args[0]
-
-    def unary_op(self, args):
-        return str(args[0]+args[1])
-
-    def string(self, args):
-        # pass
-        return args[0][1:-1] # the '[1:-1]' is to remove the double quotes
-
-    def concat_expr(self, args):
-        # pass
-        print(args[0])
-        print(args[1])
-        # return str('"'+args[0]+args[1]+'"')
-        return str(args[0]+args[1])
-    
-print(formula.pretty())
-
 evaluation = EvalExpressions().transform( formula )
-# print('below should be:', eval(contents[1:]))
-print('evaluation:', evaluation)
 
-
-''' to implement:
-'''
-        
-''' example code from the documentation:
-t = Tree('a', [Tree('expr', ['1+2'])])
-print(t)
-print(EvalExpressions().transform(t))
-'''
+print(formula.pretty())
+print('evaluation should be:\t', eval(contents[1:]))
+print('evaluation gives:\t', evaluation)
+"""

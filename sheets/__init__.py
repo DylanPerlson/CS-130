b1ea3b53b9972632d657183466753d9438ee0ded
dperlson <dperlson@ucsd.edu>
2022-02-03 14:05:01 -0800

    fix version number

--- sheets/__init__.py
+++ sheets/__init__.py
@@ -1,12 +1,8 @@
 # __init__.py
 
-
-#__all__ = ["workbook", "cell_error"]
-# import decimal
-# import sheets
-# import json
-# from .sheet import Sheet
 from .workbook import Workbook
 from .cell_error import *
 
 __all__ = ['Workbook', 'CellError', 'CellErrorType']
+
+version = '1.1.0'

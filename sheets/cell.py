2546fd1fcece960079d86824cfbde3a9b76e6b68
Pieter Van Santvliet <pietervs1@hotmail.com>
2022-02-02 18:24:31 -0800

    Add numbers to identify a specific error

--- sheets/cell.py
+++ sheets/cell.py
@@ -78,7 +78,7 @@ class Cell():
             #     evaluation = CellError(CellErrorType.TYPE_ERROR, "Incompatible types for operation")
 
             else:
-                evaluation = CellError(CellErrorType.BAD_REFERENCE, "Invalid Cell Reference", None)
+                evaluation = CellError(CellErrorType.BAD_REFERENCE, "206: Invalid Cell Reference", None)
         
 
 

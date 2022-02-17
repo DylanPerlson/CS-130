32391c330c44579525829025497d6102f80f44c5
Pieter <pietervs1@hotmail.com>
2022-02-16 21:04:10 -0800

    Move tests to project1 file

--- tests/tests_for_pieter.py
+++ tests/tests_for_pieter.py
@@ -7,15 +7,9 @@ import unittest
 D = Decimal # D(1) is now equivalent to decimal.Decimal(1)
 
 class Pieter(unittest.TestCase):
-    def test_minus_address(self):
-        wb = Workbook()
-        (_, sheet) = wb.new_sheet()
-        wb.set_cell_contents(sheet,'A1','=1')   
-        wb.set_cell_contents(sheet,'A2','=-A1')
-        self.assertEqual(D(-1), wb.get_cell_value(sheet, 'a2'))
+    pass
 
-    def test_sheet_extent_shrinking(self):
-        pass
+
 
 
 

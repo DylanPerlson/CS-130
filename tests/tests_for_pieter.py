19a0ee3a3435786ffaa9095a0cf69f38a3993549
Pieter <pietervs1@hotmail.com>
2022-02-16 20:49:50 -0800

    Add test for negative addresses

--- tests/tests_for_pieter.py
+++ tests/tests_for_pieter.py
@@ -1,11 +1,21 @@
 import context
 from sheets import *
-import decimal
+from decimal import Decimal
 import unittest
 
 
+D = Decimal # D(1) is now equivalent to decimal.Decimal(1)
+
 class Pieter(unittest.TestCase):
-    pass
+    def test_minus_address(self):
+        wb = Workbook()
+        (_, sheet) = wb.new_sheet()
+        wb.set_cell_contents(sheet,'A1','=1')   
+        wb.set_cell_contents(sheet,'A2','=-A1')
+        self.assertEqual(D(-1), wb.get_cell_value(sheet, 'a2'))
+
+    def test_sheet_extent_shrinking(self):
+        pass
 
 
 

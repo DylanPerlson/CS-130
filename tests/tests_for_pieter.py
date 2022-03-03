115aca7eb7e22b2078593b07e48b583de85fd6b8
Pieter <pietervs1@hotmail.com>
2022-03-03 11:56:19 -0800

    Move tests for better readability

--- tests/tests_for_pieter.py
+++ tests/tests_for_pieter.py
@@ -8,6 +8,7 @@ import os; os.system('clear')
 D = Decimal # D(1) is now equivalent to decimal.Decimal(1)
 
 class Pieter(unittest.TestCase):
+    pass
     # TODO is this supposed to be supported
     # def test_double_quote_literal(self):
     #     wb = Workbook()
@@ -24,78 +25,11 @@ class Pieter(unittest.TestCase):
 
     #     wb.set_cell_contents(sh, 'a1', '=AND(5 < 6 , XOR(True,false,6), OR(NOT(EXACT(true, false)), XOR(true, false, true)))')
     #     print(wb.get_cell_value(sh, 'A1'))
+    #     wb.set_cell_contents(sh, 'a1', '=AND(true, false)')
+    #     print(wb.get_cell_value(sh, 'A1'))
 
-    def test_lark_func_dir(self):
-        wb = Workbook()
-        (_,sh) = wb.new_sheet()
 
-        wb.set_cell_contents(sh, 'a1', '=AND(true, false)')
-        #print(wb.get_cell_value(sh, 'A1'))
 
-    def test_lark_bool_lit(self):
-        wb = Workbook()
-        (_,sh) = wb.new_sheet()
-
-        wb.set_cell_contents(sh, 'a1', '=true')
-        self.assertEqual(True, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '=FaLse')
-        self.assertEqual(False, wb.get_cell_value(sh, 'a1'))
-
-    def test_bool_oper(self):
-        wb = Workbook()
-        (_, sh) = wb.new_sheet()
-
-        # stuff that should be True
-
-        wb.set_cell_contents(sh, 'a1', '=true')
-        self.assertEqual(True, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '=5=5')
-        self.assertEqual(True, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '="e" = "e"')
-        self.assertEqual(True, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '="e" = "E"')
-        self.assertEqual(True, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '=false < true')
-        self.assertEqual(True, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '="BLUE" = "blue"')
-        self.assertEqual(True, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '="12" > 12')
-        self.assertEqual(True, wb.get_cell_value(sh, 'a1'))
-
-        # stuff that should be False
-
-        wb.set_cell_contents(sh, 'a1', '=(5 = 4)')
-        self.assertEqual(False, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '=5 != 5')
-        self.assertEqual(False, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '="BLUE" < "blue"')
-        self.assertEqual(False, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '="a" < "["')
-        self.assertEqual(False, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '="TRUE" > FALSE')
-        self.assertEqual(False, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '=faLse > true')
-        self.assertEqual(False, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'a1', '=true == false')
-        self.assertEqual(False, wb.get_cell_value(sh, 'a1'))
-
-        wb.set_cell_contents(sh, 'b1', 'hello world')
-        wb.set_cell_contents(sh, 'b2', "hello")
-        wb.set_cell_contents(sh, 'a1', '=b1 = B2 & " world"')
-        self.assertEqual(True, wb.get_cell_value(sh, 'a1'))
 
 
 

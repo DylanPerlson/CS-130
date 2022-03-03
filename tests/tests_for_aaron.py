e8d4c17cc01badd329d1c151863eb7515a194367
Aaron Tran <aaron.ctran@gmail.com>
2022-03-03 11:48:25 -0800

    Fix assertion statements for tests

--- tests/tests_for_aaron.py
+++ tests/tests_for_aaron.py
@@ -49,10 +49,10 @@ class Aaron(unittest.TestCase):
         wb.set_cell_contents(name,'A5',"=AND(false, false)")
         wb.set_cell_contents(name,'A6',"=AND(false, true)")
 
-        self.assertEqual(wb.get_cell_value(name, 'A3'), "false")
-        self.assertEqual(wb.get_cell_value(name, 'A4'), "true")
-        self.assertEqual(wb.get_cell_value(name, 'A5'), "false")
-        self.assertEqual(wb.get_cell_value(name, 'A6'), "false")
+        self.assertEqual(wb.get_cell_value(name, 'A3'), False)
+        self.assertEqual(wb.get_cell_value(name, 'A4'), True)
+        self.assertEqual(wb.get_cell_value(name, 'A5'), False)
+        self.assertEqual(wb.get_cell_value(name, 'A6'), False)
     
     def test_or(self):
         wb = Workbook()
@@ -62,10 +62,10 @@ class Aaron(unittest.TestCase):
         wb.set_cell_contents(name,'A5',"=OR(false, false)")
         wb.set_cell_contents(name,'A6',"=OR(false, true)")
 
-        self.assertEqual(wb.get_cell_value(name, 'A3'), "true")
-        self.assertEqual(wb.get_cell_value(name, 'A4'), "true")
-        self.assertEqual(wb.get_cell_value(name, 'A5'), "false")
-        self.assertEqual(wb.get_cell_value(name, 'A6'), "true")
+        self.assertEqual(wb.get_cell_value(name, 'A3'), True)
+        self.assertEqual(wb.get_cell_value(name, 'A4'), True)
+        self.assertEqual(wb.get_cell_value(name, 'A5'), False)
+        self.assertEqual(wb.get_cell_value(name, 'A6'), True)
     
     def test_not(self):
         wb = Workbook()
@@ -73,19 +73,19 @@ class Aaron(unittest.TestCase):
         wb.set_cell_contents(name,'A3',"=NOT(true)")
         wb.set_cell_contents(name,'A4',"=NOT(false)")
 
-        self.assertEqual(wb.get_cell_value(name, 'A3'), "false")
-        self.assertEqual(wb.get_cell_value(name, 'A4'), "true")
+        self.assertEqual(wb.get_cell_value(name, 'A3'), False)
+        self.assertEqual(wb.get_cell_value(name, 'A4'), True)
     
     def test_exact(self):
         wb = Workbook()
         (_, name) = wb.new_sheet("s1")
-        wb.set_cell_contents(name,'A3',"=EXACT('hello', 'HELLO')")
-        wb.set_cell_contents(name,'A4',"=EXACT('Hello','Hello')")
-        wb.set_cell_contents(name,'A5',"=EXACT('Hello', 'hElLo')")
+        wb.set_cell_contents(name,'A3',"=EXACT(hello, HELLO)")
+        wb.set_cell_contents(name,'A4',"=EXACT(Hello,Hello)")
+        wb.set_cell_contents(name,'A5',"=EXACT(Hello, hElLo)")
 
-        self.assertEqual(wb.get_cell_value(name, 'A3'), "false")
-        self.assertEqual(wb.get_cell_value(name, 'A4'), "true")
-        self.assertEqual(wb.get_cell_value(name, 'A5'), "false")
+        self.assertEqual(wb.get_cell_value(name, 'A3'), False)
+        self.assertEqual(wb.get_cell_value(name, 'A4'), True)
+        self.assertEqual(wb.get_cell_value(name, 'A5'), False)
 
 
 

8602fd24d50384b198dcc48ebe87fabfff9b23b6
Pieter <pietervs1@hotmail.com>
2022-03-03 23:11:38 -0800

    Reorder imports

--- tests/tests_all.py
+++ tests/tests_all.py
@@ -1,11 +1,12 @@
 import unittest
+
+from tests_for_aaron import Aaron
+from tests_for_dylan import Dylan
+from tests_for_pieter import Pieter
 from tests_project1 import Project1
 from tests_project2 import Project2
 from tests_project3 import Project3
 from tests_project4 import Project4
-from tests_for_aaron import Aaron
-from tests_for_dylan import Dylan
-from tests_for_pieter import Pieter
 
 import os; os.system('clear')
 

508d2ccc5e69e317ea9c8be6ec8de4894d10f02e
dperlson <dperlson@ucsd.edu>
2022-03-04 18:50:43 -0800

    update version number

--- sheets/functions.py
+++ sheets/functions.py
@@ -154,7 +154,7 @@ class Functions:
         Returns version number."""
         if len(args) != 0:
             return CellError(CellErrorType.TYPE_ERROR, "Invalid arguments")
-        return '1.3.0' #sheet.version
+        return '1.3' #sheet.version
 
     def indirect_func(self, args):
         """The cell value is returned."""
